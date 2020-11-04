af80fc6d39e2aec051201121eac489a32385de2a
djordje_radivojevic_eng <djordje.radivojevic.eng@gmail.com>
2020-11-04 08:44:26 +0100

    Initial commit

---
+++ src/main/java/com/springframework/spring5restmvc/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.springframework.spring5restmvc.repositories;
+
+import com.springframework.spring5restmvc.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category,Long> {
+}
---
+++ src/test/java/com/springframework/spring5restmvc/Spring5RestMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.spring5restmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5RestMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
